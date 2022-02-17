04c3fcb49e39980cb56db1e7b67fbc950a5cbeb3
brknkl <berkanankal68@gmail.com>
2022-02-17 16:53:08 +0300

    Password hash with bcryptjs

--- models/User.js
+++ models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
 const { Schema } = mongoose;
 
@@ -43,6 +44,19 @@ const UserSchema = new Schema({
   about: String,
   website: String,
   place: String,
+});
+
+UserSchema.pre("save", function (next) {
+  if (!this.isModified("password")) return next();
+
+  bcrypt.genSalt(10, (err, salt) => {
+    if (err) return next(err);
+    bcrypt.hash(this.password, salt, (err, hash) => {
+      if (err) return next(err);
+      this.password = hash;
+      return next();
+    });
+  });
 });
 
 module.exports = mongoose.model("User", UserSchema);
