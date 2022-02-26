4626c886485304023c39956e9a97a1b3470ba254
brknkl <berkanankal68@gmail.com>
2022-02-26 10:19:47 +0300

    Get all answers by question

--- controllers/answers.js
+++ controllers/answers.js
@@ -1,6 +1,6 @@
 const asyncHandler = require("express-async-handler");
-const CustomError = require("../helpers/error/CustomError");
 const Answer = require("../models/Answer");
+const Question = require("../models/Question");
 
 const addNewAnswerToQuestion = asyncHandler(async (req, res, next) => {
   const { content } = req.body;
@@ -19,4 +19,19 @@ const addNewAnswerToQuestion = asyncHandler(async (req, res, next) => {
   });
 });
 
-module.exports = { addNewAnswerToQuestion };
+const getAllAnswersByQuestion = asyncHandler(async (req, res, next) => {
+  const { question_id } = req.params;
+
+  // const answers = await Answer.find({ question: question_id });
+
+  const question = await Question.findById(question_id).populate("answers");
+  const answers = question.answers;
+
+  return res.status(200).json({
+    success: true,
+    count: answers.length,
+    data: answers,
+  });
+});
+
+module.exports = { addNewAnswerToQuestion, getAllAnswersByQuestion };
--- routes/answers.js
+++ routes/answers.js
@@ -1,9 +1,13 @@
 const express = require("express");
-const { addNewAnswerToQuestion } = require("../controllers/answers");
+const {
+  addNewAnswerToQuestion,
+  getAllAnswersByQuestion,
+} = require("../controllers/answers");
 const { getAccessToRoute } = require("../middlewares/authorization/auth");
 
 const router = express.Router({ mergeParams: true });
 
 router.post("/", getAccessToRoute, addNewAnswerToQuestion);
+router.get("/", getAllAnswersByQuestion);
 
 module.exports = router;
